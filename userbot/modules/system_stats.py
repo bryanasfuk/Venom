8c9ea4b9c6c2a2487c06e310f361babe5a246a06
bryanasfuk <80415153+bryanasfuk@users.noreply.github.com>
2021-03-11 01:54:40 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -164,12 +164,12 @@ async def amireallyalive(alive):
     logo = ALIVE_LOGO
     output = (
         f"`Venom Is On Fire 🔥 `{repo.active_branch.name}`\n"
-        "`━━━━━━━━━━━━━━━━━━━━━━━━━━━`\n"
-        f"😈 Boss  : {DEFAULTUSER}\n\n"
-        f"😈 Python : {python_version()}\n\n"
-        f"😈 Tele  : {version.__version__}\n\n"
-        f"😈 Bot On fire  : {uptime}\n"
-        "`━━━━━━━━━━━━━━━━━━━━━━━━━━━`\n"
+        "`┏━━━━━━━━━━━━━━━━━━━━━━━━━━━`\n"
+        f"┣• Boss    : {DEFAULTUSER}\n\n"
+        f"┣• Python  : {python_version()}\n\n"
+        f"┣• Tele    : {version.__version__}\n\n"
+        f"┣• Menyala : {uptime}\n"
+        "`┗━━━━━━━━━━━━━━━━━━━━━━━━━━━\n"
     )
     if ALIVE_LOGO:
         try:
